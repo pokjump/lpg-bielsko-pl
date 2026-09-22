dd79f4b0b4fa753714a2fc0dd0b0ce9a3bf4838f
pokjump <filips12filips@gmail.com>
2026-09-22 12:56:56 +0200

    wylacz pliki robocze z repo

--- unused/js/canonical-fix.js
+++
@@ -1,8 +0,0 @@
-(function () {
-  if (location.hostname.indexOf("lpg.bielsko.pl") !== -1) return;
-  var origin = location.origin + "/";
-  var canonical = document.querySelector('link[rel="canonical"]');
-  if (canonical) canonical.setAttribute("href", origin);
-  var ogUrl = document.querySelector('meta[property="og:url"]');
-  if (ogUrl) ogUrl.setAttribute("content", origin);
-})();
